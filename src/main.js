5da08a35fb11c701885e8740a45e513bc7d71259
dp <dpronin@yandex.ru>
2021-01-17 15:12:37 -0800

    testing pwa

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ Vue.config.devtools = true;
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
+import './registerServiceWorker'
 
 Vue.use(VueMaterial)
 
